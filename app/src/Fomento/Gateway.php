842b06cb21c653dc3694114b14de5100258e1e3b
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-09-13 13:15:48 +0200

    Testing that the signature is being performed

--- app/src/Fomento/Gateway.php
+++ app/src/Fomento/Gateway.php
@@ -6,16 +6,22 @@ use SoapClient;
 
 class Gateway
 {
-
     private SoapClient $client;
+    private Signer $signer;
 
-    public function __construct(SoapClient $client)
+    public function __construct(SoapClient $client, Signer $signer)
     {
         $this->client = $client;
+        $this->signer = $signer;
     }
 
     public function getFunctions(): ?array
     {
         return $this->client->__getFunctions();
+    }
+
+    public function call(string $method, string $xmlText)
+    {
+        $this->signer->sign($xmlText);
     }
 }
--- app/test/GatewayShould.php
+++ app/test/GatewayShould.php
@@ -3,22 +3,82 @@
 namespace Fomento\Test;
 
 use Fomento\Gateway;
+use Fomento\Signer;
 use PHPUnit\Framework\TestCase;
+use SimpleXMLElement;
 use SoapClient;
 
 class GatewayShould extends TestCase
 {
+    private Signer $signer;
+    private SoapClient $soapClient;
+
+    public function setUp(): void
+    {
+        $this->signer = $this->createMock(Signer::class);
+        $this->soapClient = $this->createMock(SoapClient::class);
+    }
+
     /**
      * @test
      */
     public function uses_the_webservice(): void
     {
-        $client = $this->createMock(SoapClient::class);
-        $client
+        $this
+            ->soapClient
             ->expects($this->once())
             ->method('__getFunctions');
 
-        $gateway = new Gateway($client);
+        $gateway = $this->buildGateway();
         $gateway->getFunctions();
+    }
+
+    /**
+     * @param string $method
+     * @test
+     * @return void
+     * @dataProvider methodNamesProvider
+     */
+    public function sign_the_xml_it_receives(string $method): void
+    {
+        $xml = new SimpleXMLElement('<root/>');
+        $xml->addChild('aChild');
+
+        $xmlText = $xml->asXML();
+
+        $this->signer
+            ->expects($this->once())
+            ->method('sign')
+            ->with($xmlText);
+
+        $gateway = $this->buildGateway();
+        $method = "aMethod";
+
+        $gateway->call($method, $xmlText);
+    }
+
+    public function call_the_method_it_receives(): void
+    {
+    }
+
+    public function return_the_raw_xml(): void
+    {
+    }
+
+    public function methodNamesProvider(): array
+    {
+        return
+            [
+                ['aMethod',],
+                ['ConsultaDeServicio',],
+            ];
+    }
+
+    /**
+     * @return Gateway
+     */
+    private function buildGateway(): Gateway
+    {
+        return new Gateway($this->soapClient, $this->signer);
     }
 }
