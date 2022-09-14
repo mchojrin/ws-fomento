69b3878f36e1a1cd541dfb21e9c3de60e5e6cd0c
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-09-14 10:58:25 +0200

    Adding an XML signing library

--- app/src/Fomento/Signer.php
+++ app/src/Fomento/Signer.php
@@ -2,10 +2,24 @@
 
 namespace Fomento;
 
+use XmlDsig\XmlDigitalSignature;
+
 class Signer
 {
+    private XmlDigitalSignature $digitalSignature;
+
+    public function __construct(string $privateKeyPath, string $publicKeyPath, string $privateKeyPassphrase)
+    {
+        $this->digitalSignature = new XmlDigitalSignature();
+        $this->digitalSignature->loadPrivateKey($privateKeyPath);
+        $this->digitalSignature->loadPublicKey($publicKeyPath);
+    }
+
     public function sign(string $text): string
     {
-        return $text;
+        $this->digitalSignature->addObject($text);
+        $this->digitalSignature->sign();
+
+        return $this->digitalSignature->getSignedDocument();
     }
 }
